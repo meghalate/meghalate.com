b284ded5ba76c92c73a0905135b81c6150d06ae5
Megha Late <meghalate@Hrushikeshs-MacBook-Pro.local>
2015-04-16 00:28:05 -0700

    changed port

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express=require('express');
     var app=express();
 
-app.use(express.static('/public'));
-app.set('port', (process.env.PORT || 3000));
+app.use(express.static('public'));
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
 	console.log("Node app is running at localhost:" + app.get('port'))
